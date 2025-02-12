90d2894b1826e3eee071c60646c382f697da80fd
Evans Tucker <evans.tucker@dfinity.org>
2025-02-12 12:20:05 -0800

    Fixing api

--- api/app.py
+++ api/app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hey_there():
-    return "Try /heytherefella"
-
-@app.route('/heytherefella')
-def hey_there():
-    return "Whose soul are you tormenting now?"
+    return "TOTALLY WORKING API"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
